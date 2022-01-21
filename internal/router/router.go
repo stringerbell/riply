dd9eeea8a2cf35ae16bb58cf40717779d97ed964
Dann Stockton <dannstockton@gmail.com>
2022-01-20 19:38:27 -0700

    Add routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,10 +2,19 @@ package router
 
 import (
   "github.com/gorilla/mux"
+  "net/http"
+  "range/internal"
+  "range/internal/handlers"
+  "range/internal/storage"
 )
 
 
 func New(env internal.Env) *mux.Router {
   r := mux.NewRouter()
+  sto := storage.NewShortLinkStorage(env.DB)
+  r.HandleFunc("/new", handlers.NewShortLinkHandler(sto, env.Cfg)).Methods(http.MethodPost)
+  r.HandleFunc("/{shortlink}+", handlers.NewStatsHandler(sto)).Methods(http.MethodGet)
+  r.HandleFunc("/{shortlink}", handlers.NewShortLinkForwarder(sto)).Methods(http.MethodGet)
+
   return r
 }
